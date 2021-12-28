97e48a4cde864a92ec82a8bbdd7d20aebbcb1607
jendawkins <35514979+jendawkins@users.noreply.github.com>
2021-12-28 16:16:18 -0700

    dispatcher

--- dispatcher_MAP.py
+++ dispatcher_MAP.py
@@ -56,7 +56,7 @@ echo $TMPDIR
 cd /PHShome/jjd65/m2m_map
 rm *.err
 rm *.out
-python3 ./main_MAPv0.py -learn {0} -priors {1} -N_met {3} -N_bug {4} -L {6} -K {7} -meas_var {8} -prior_meas_var {9} -seed {10}
+python3 ./main_MAP.py -learn {0} -priors {1} -N_met {3} -N_bug {4} -L {6} -K {7} -meas_var {8} -prior_meas_var {9} -seed {10}
 '''
 
 N_met = 20
