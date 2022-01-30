758c84f39ecd35e78aa07adca48ed78083ecfa38
jendawkins <35514979+jendawkins@users.noreply.github.com>
2022-01-30 17:10:14 -0500

    dispatcher

--- dispatcher_MAP.py
+++ dispatcher_MAP.py
@@ -55,27 +55,93 @@ echo $TMPDIR
 # source activate dispatcher
 # module load Anaconda3/5.2.0
 cd /PHShome/jjd65/m2m_map
-python3 ./main_MAP.py -learn {0} -priors {1} -N_met {2} -N_bug {3} -L {4} -K {5} -meas_var {6} -seed {7} -load {8} -case {9} -iterations {10}
+# python3 ./main_MAP.py -learn {0} -priors {1} -N_met {2} -N_bug {3} -L {4} -K {5} -meas_var {6} -seed {7} -load {8} -case {9} -iterations {10}
+python3 ./main_MAP.py
 '''
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-load", "--load", help="load or not", type=int)
 parser.add_argument("-case", "--case", help="case", type=str)
 parser.add_argument("-iter", "--iter", help="iter", type=int)
 args = parser.parse_args()
+# prior_meas_var = 1e6
+# meas_var = 0.01
+# local = 1
+# learn_num_clusters = 1
+# learn_list = ['beta','alpha',('r_met', 'mu_met', 'z'),('r_bug','mu_bug'),('pi_met','z')]
+learn_list = [('beta','alpha','r_met','mu_met','z','pi_met','e_met'),('beta','alpha','r_met','mu_met','z','pi_met','e_met')]
+priors_list = [('beta','alpha','r_met','mu_met','z','pi_met','e_met'),('beta','alpha','r_met','mu_met','z','pi_met')]
+param_dict1 = {'N_met': 20, 'N_bug': 20, ('L', 'K'): [(2,2),(3,3)], 'seed': [0,1,2], 'hyper_mu': [0,1], 'hyper_r': [0,1],
+              ('learn', 'priors'): list(zip(*[learn_list, priors_list])), 'lr': 0.01, 'iter': 20001, 'load': 1, 'lm': 1,
+               'linear': 1}
 
-N_met = 20
-N_bug = 20
-learn = 'all'
-pid_list = []
-meas_var = 0.01
-priors = 'all'
-for seed in range(10):
-    # for meas_var in [0.01, 1]:
-    for L, K in [(3,3),(4,4)]:
-        # for repeat_clusters in [0, 1]:
-        for N_met, N_bug in [(25,25)]:
-            f = open('m2m.lsf', 'w')
-            f.write(my_str.format(learn, priors, N_met, N_bug, L, K, meas_var, seed, args.load, args.case, args.iter))
-            f.close()
-            os.system('bsub < {}'.format('m2m.lsf'))
+learn_list = ['', 'all']
+priors_list = ['', 'all']
+param_dict2 = {'N_met': 20, 'N_bug': 20, ('L', 'K'): [(2,2)], 'seed': [0,1,2], 'linear': 0,
+              ('learn', 'priors'): list(zip(*[learn_list, priors_list])), 'lr': 0.01, 'iter': 20001, 'load': 1, 'lm': 0}
+
+for param_dict in [param_dict1, param_dict2]:
+    total_iters = np.prod([len(v) for v in param_dict.values() if hasattr(v, '__len__')])
+
+    i = 0
+    list_keys = []
+    list_vals = []
+    for key, value in param_dict.items():
+        list_keys.append(key)
+        if hasattr(value, "__len__"):
+            list_vals.append(value)
+        else:
+            list_vals.append([value])
+
+    zipped_params = list(itertools.product(*list_vals))
+
+    pid_list = []
+    for p in zipped_params:
+        fin_list = []
+        i = 0
+        for l in list_keys:
+            if hasattr(p[i], '__len__'):
+                for ii in range(len(p[i])):
+                    if isinstance(p[i][ii], tuple):
+                        fin = ' '.join(p[i][ii])
+                    else:
+                        fin = p[i][ii]
+                    my_str = my_str + ' -' + l[ii] + ' ' + str(fin)
+            else:
+                my_str = my_str + ' -' + l + ' ' + str(p[i])
+            i += 1
+        if args.case:
+            my_str = my_str + ' -case ' + args.case
+        if args.load:
+            my_str = my_str + '-load ' + args.load
+        if args.iter:
+            my_str = my_str + '-iter ' + args.iter
+        cmd = my_str
+        f = open('m2m.lsf', 'w')
+        f.write(my_str)
+        f.close()
+        os.system('bsub < {}'.format('m2m.lsf'))
+
+
+# parser = argparse.ArgumentParser()
+# parser.add_argument("-load", "--load", help="load or not", type=int)
+# parser.add_argument("-case", "--case", help="case", type=str)
+# parser.add_argument("-iter", "--iter", help="iter", type=int)
+# args = parser.parse_args()
+#
+# N_met = 20
+# N_bug = 20
+# learn = 'all'
+# pid_list = []
+# meas_var = 0.01
+# priors = 'all'
+# for seed in range(10):
+#     # for meas_var in [0.01, 1]:
+#     for L, K in [(3,3),(4,4)]:
+#         # for repeat_clusters in [0, 1]:
+#         for N_met, N_bug in [(25,25)]:
+#             f = open('m2m.lsf', 'w')
+#             f.write(my_str.format(learn, priors, N_met, N_bug, L, K, meas_var, seed, args.load, args.case, args.iter))
+#             f.close()
+#             os.system('bsub < {}'.format('m2m.lsf'))
