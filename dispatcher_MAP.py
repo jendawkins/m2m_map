4cc50d9e1c4f9b51361b57c2a144d8df0ac33b7b
jendawkins <35514979+jendawkins@users.noreply.github.com>
2022-01-31 10:22:35 -0500

    dispatcher

--- dispatcher_MAP.py
+++ dispatcher_MAP.py
@@ -1,6 +1,6 @@
 import subprocess
 from main_MAPv0 import *
-my_str = '''
+my_str_orig = '''
 #!/bin/bash
 #BSUB -J m2m
 #BSUB -o case{9}_L{4}_K{5}.out
@@ -55,16 +55,15 @@ echo $TMPDIR
 # source activate dispatcher
 # module load Anaconda3/5.2.0
 cd /PHShome/jjd65/m2m_map
-# python3 ./main_MAP.py -learn {0} -priors {1} -N_met {2} -N_bug {3} -L {4} -K {5} -meas_var {6} -seed {7} -load {8} -case {9} -iterations {10}
-python3 ./main_MAP.py
 '''
-
+# python3 ./main_MAP.py -learn {0} -priors {1} -N_met {2} -N_bug {3} -L {4} -K {5} -meas_var {6} -seed {7} -load {8} -case {9} -iterations {10}
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-load", "--load", help="load or not", type=int)
 parser.add_argument("-case", "--case", help="case", type=str)
 parser.add_argument("-iter", "--iter", help="iter", type=int)
 args = parser.parse_args()
+
 # prior_meas_var = 1e6
 # meas_var = 0.01
 # local = 1
@@ -79,7 +78,8 @@ param_dict1 = {'N_met': 20, 'N_bug': 20, ('L', 'K'): [(2,2),(3,3)], 'seed': [0,1
 learn_list = ['', 'all']
 priors_list = ['', 'all']
 param_dict2 = {'N_met': 20, 'N_bug': 20, ('L', 'K'): [(2,2)], 'seed': [0,1,2], 'linear': 0,
-              ('learn', 'priors'): list(zip(*[learn_list, priors_list])), 'lr': 0.01, 'iter': 20001, 'load': 1, 'lm': 0}
+              ('learn', 'priors'): list(zip(*[learn_list, priors_list])), 'lr': 0.01, 'iter': 20001, 'load': 1, 'lm': 0,
+               'nltype': ['linear', 'sin', 'exp']}
 
 for param_dict in [param_dict1, param_dict2]:
     total_iters = np.prod([len(v) for v in param_dict.values() if hasattr(v, '__len__')])
@@ -89,7 +89,7 @@ for param_dict in [param_dict1, param_dict2]:
     list_vals = []
     for key, value in param_dict.items():
         list_keys.append(key)
-        if hasattr(value, "__len__"):
+        if hasattr(value, "__len__") and not isinstance(value, str):
             list_vals.append(value)
         else:
             list_vals.append([value])
@@ -100,8 +100,9 @@ for param_dict in [param_dict1, param_dict2]:
     for p in zipped_params:
         fin_list = []
         i = 0
+        my_str = "python3 ./main_MAP.py"
         for l in list_keys:
-            if hasattr(p[i], '__len__'):
+            if hasattr(p[i], '__len__') and not isinstance(p[i], str):
                 for ii in range(len(p[i])):
                     if isinstance(p[i][ii], tuple):
                         fin = ' '.join(p[i][ii])
@@ -119,8 +120,9 @@ for param_dict in [param_dict1, param_dict2]:
             my_str = my_str + '-iter ' + args.iter
         cmd = my_str
         print(cmd)
+        print(my_str_orig + my_str)
         f = open('m2m.lsf', 'w')
-        f.write(my_str)
+        f.write(my_str_orig + my_str)
         f.close()
         os.system('bsub < {}'.format('m2m.lsf'))
 
